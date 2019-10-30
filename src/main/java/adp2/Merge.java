5b1c459fb5f0ad1c53bf8a51805a9fa4973d1b65
juanitolaguna <little.pilgrim@mail.ru>
2019-10-30 12:42:24 +0100

    wip

--- src/main/java/adp2/Merge.java
+++ src/main/java/adp2/Merge.java
@@ -1,15 +1,17 @@
 package adp2;
 
+import java.util.Arrays;
+
 /**
  * Merge
  */
 public class Merge {
 
   /**
-   * Top Down
+   * Top down Merge Sort
    * 
    * @param queue
-   * @return
+   * @return queue
    */
   public static Queue<Comparable> sortTD(Queue<Comparable> queue) {
     int size = queue.size();
@@ -26,23 +28,32 @@ public class Merge {
     while (!queue.isEmpty()) {
       q2.enqueue(queue.dequeue());
     }
-
     return merge(sortTD(q1), sortTD(q2));
   }
 
-  public static Queue sortBU(Queue<Comparable> queue) {
-    Queue<Queue> q = new Queue();
 
+  /**
+   * Bottom Up merge sort
+   * @param queue
+   * @return queue
+   */
+  public static Queue sortBU(Queue<Comparable> queue) {
+    Queue<Queue<Comparable>> q = new Queue<>();
+
+    // Erzeugen Sie N-Warteschlangen für N-Elemente
     queue.forEach(e -> {
       Queue<Comparable> tmp = new Queue<>();
       tmp.enqueue(e);
-      // System.out.println("Tmp: " + tmp);
       q.enqueue(tmp);
       System.out.println("q: " + q);
     });
 
-    while (q.size() > 1)
+    //Mischen Sie jeweils die ersten beiden Warteschlangen
+    // und legen das Ergebnis wieder in der Warteschlange ab.
+    while (q.size() > 1) {
+
       q.enqueue(merge(q.dequeue(), q.dequeue()));
+    }
 
     return q;
 
@@ -66,20 +77,44 @@ public class Merge {
     return merged;
   }
 
-  public static Queue<Comparable> merge1(Queue<Comparable> a, Queue<Comparable> b) {
-    Queue<Comparable> c = new Queue<>();
-    while (!a.isEmpty() || !b.isEmpty()) {
-      if (a.isEmpty())
-        c.enqueue(b.dequeue());
-      else if (b.isEmpty())
-        c.enqueue(a.dequeue());
-      else if (a.peek().compareTo(b.peek()) < 0)
-        c.enqueue(a.dequeue());
+
+  /**
+   * Merge aLeft & aRight into array
+   * @param aLeft
+   * @param aRight
+   * @param array
+   */
+  public static void mergeTDAry(Comparable[] aLeft, Comparable[] aRight, Comparable[] array) {
+    int i = 0, j = 0;
+    while (i + j < array.length) {
+      if (j == aRight.length || (i < aLeft.length && less(aLeft[i], aRight[j])))
+        // copy ith element of aLeft and increment i
+        array[i+j] = aLeft[i++];
       else
-        c.enqueue(b.dequeue());
+        // copy jth element of a2 and increment j
+        array[i+j] = aRight[j++];
     }
-    return c;
   }
+
+//  /** Merge-sort contents of array S. */
+//  public static <K> void mergeSort(K[] S) {
+//    int n = S.length;
+//    if (n < 2) return;                        // array is trivially sorted
+//    // divide
+//    int mid = n/2;
+//    K[] S1 = Arrays.copyOfRange(S, 0, mid);   // copy of first half
+//    K[] S2 = Arrays.copyOfRange(S, mid, n);   // copy of second half
+//    // conquer (with recursion)
+//    mergeSort(S1);                      // sort copy of first half
+//    mergeSort(S2);                      // sort copy of second half
+//    // merge results
+//    mergeTDAry(S1, S2, S);               // merge sorted halves back into original
+//  }
+
+
+
+
+
 
   private static boolean less(Comparable v, Comparable w) {
     return v.compareTo(w) < 0;
@@ -87,7 +122,6 @@ public class Merge {
 
   public static void main(String[] args) {
     Queue<Comparable> q1 = new Queue<>();
-    Queue<Comparable> q2 = new Queue<>();
 
     q1.enqueue(1);
     q1.enqueue(2);
@@ -99,10 +133,6 @@ public class Merge {
     q1.enqueue(80);
     q1.enqueue(11);
 
-    // q2.enqueue(5);
-    // q2.enqueue(6);
-    // q2.enqueue(7);
-    
     System.out.println(sortBU(q1));
   }
 }
