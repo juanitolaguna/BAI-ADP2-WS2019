537371264d9a4674a545c6aa38b16787f51078d9
Miguel Sanchez Rueda <miguel.sanchez@hotmail.de>
2019-12-09 14:33:34 +0100

    wip...

--- src/main/java/adp2/MergeScratch.java
+++ src/main/java/adp2/MergeScratch.java
@@ -1,31 +1,11 @@
-//package adp2;
-//
-//public class MergeScratch {
-//    /**
-//     * [ 1 2 3 4 | 5 6 7 8 ]
-//     */
-//
-//    public static void merge(Comparable[] array, Comparable[] aux, int lo, int mid, int hi) {
-//        int i = lo;
-//        int j = mid + 1;
-//
-//        // copy into aux array
-//        for (int n = lo; n <= hi; n++) {
-//            aux[n] = array[n];
-//        }
-//
-//        for (int n = lo; n <= hi; n++) {
-//            if (i > mid) {
-//                array[n] = aux[j++];
-//            } else if (j > hi) {
-//                array[n] = aux[i++];
-//            } else if (getLess(aux[j], aux[i])) {
-//                array[n] = aux[j++];
-//            } else {
-//                array[n] = aux[i++];
-//            }
-//        }
-//
-//
-//    }
-//}
+package adp2;
+
+import java.util.PriorityQueue;
+
+public class MergeScratch {
+
+    public static void main(String[] args) {
+        PriorityQueue prio = new PriorityQueue();
+
+    }
+}
---
+++ src/test/java/adp2/VectorADTTest.java
@@ -0,0 +1,16 @@
+package adp2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class VectorADTTest {
+
+    @Test
+     public void euclideanSameTest() {
+        VectorADT v1 = new VectorADT(3.0, 3.0 , 3.0);
+        assertEquals(0, 0);
+
+
+    }
+}
