43d1f833ea30a3d66b1cf702bc0f87a7233067eb
juanitolaguna <little.pilgrim@mail.ru>
2020-01-06 12:37:30 +0100

    custom hashcode implementation

--- src/main/java/adp4/TestScr.java
+++ src/main/java/adp4/TestScr.java
@@ -18,19 +18,36 @@ public class TestScr {
         return hash;
     }
 
+    public static int hash(int [] intArray) {
+        int h = 0;
+        int m = intArray.length;
+        int r = 3;
+        int q = 997;
+        for (int i = 0; i < m; i++) {
+            h = (r * h + intArray[i]) % q;
+        }
+        return h;
+    }
+
     public static boolean compareString(String one, String two) {
         return true;
     }
 
     public static void main(String[] args) {
-        Character[] charList = new Character[]{'V', 'E', 'R', 'Y', 'I', 'M', 'P', 'O', 'T', 'A', 'N', 'S'};
-        for (int i = 0; i < charList.length - 1; i++) {
-            System.out.println(charList[i] + " " + charList[i].hashCode() + " -> " + hashM(charList[i]));
-            //System.out.println(hashM(charList[i]));
-        }
+//        Character[] charList = new Character[]{'V', 'E', 'R', 'Y', 'I', 'M', 'P', 'O', 'T', 'A', 'N', 'S'};
+//        for (int i = 0; i < charList.length - 1; i++) {
+//            System.out.println(charList[i] + " " + charList[i].hashCode() + " -> " + hashM(charList[i]));
+//            //System.out.println(hashM(charList[i]));
+//        }
+//
+//        String one = "AaAa";
+//        String two = "BBBB";
+//        System.out.println(hash(one) == hash(two));
 
-        String one = "AaAa";
-        String two = "BBBB";
-        System.out.println(hash(one) == hash(two));
+        int[] arry1 = {1, 3, 2, 2, 1};
+        int[] arry2 = {1, 1, 3, 2, 2};
+        System.out.println(hash(arry1));
+        System.out.println(hash(arry2));
+        System.out.println((2428 % 997));
     }
 }
